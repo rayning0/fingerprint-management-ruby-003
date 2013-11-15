91f97473186e9bbac5f57fa375b0cdf9657b56a3
Spencer Rogers <spencer1248@gmail.com>
2013-11-15 16:19:36 -0500

    Fix stuff

--- db/migrate/20131114212533_create_scanned_fingerprints.rb
+++ db/migrate/20131114212533_create_scanned_fingerprints.rb
@@ -1,10 +1,10 @@
 class CreateScannedFingerprints < ActiveRecord::Migration
   def change
     create_table :scanned_fingerprints do |t|
-      t.integer :finger_print_scanner_id
-      t.integer :finger_print_id
+      t.integer :fingerprint_scanner_id
+      t.integer :fingerprint_id
     end
-    add_index :scanned_fingerprints, :finger_print_scanner_id
-    add_index :scanned_fingerprints, :finger_print_id
+    add_index :scanned_fingerprints, :fingerprint_scanner_id
+    add_index :scanned_fingerprints, :fingerprint_id
   end
 end
