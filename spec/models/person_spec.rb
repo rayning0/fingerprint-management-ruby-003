41e9cc70f63251f8f9cade0296ef57953b6fe6f3
Spencer Rogers <spencer1248@gmail.com>
2013-11-15 11:41:22 -0500

    Fix test

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -6,15 +6,18 @@ describe "Person" do
     it "should return people with a history of the given incident" do
       robbers = Person.with_history_of("Armed Robbery")
       robbers.count.should == 2
-      robbers.should include([@kelvin_martin, @john_dillinger])
+      robbers.should include(@kelvin_martin)
+      robbers.should include(@john_dillinger)
     end
   end
 
   describe "::tracked_by" do
     it "should return people with fingerprints tracked by the given organization" do
       people_tracked_by_fbi = Person.tracked_by("Federal Bureau of Investigations")
+
       people_tracked_by_fbi.count.should == 2
-      people_tracked_by_fbi.should include([@al_capone, @john_dillinger])
+      people_tracked_by_fbi.should include(@al_capone)
+      people_tracked_by_fbi.should include(@john_dillinger)
     end
   end
 end
