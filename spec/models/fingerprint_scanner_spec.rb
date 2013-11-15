6a8e693c62599bfae0a62fcd55d4454029962741
Spencer Rogers <spencer1248@gmail.com>
2013-11-15 16:52:09 -0500

    Fix typo

--- spec/models/fingerprint_scanner_spec.rb
+++ spec/models/fingerprint_scanner_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "FingerprintScanner" do
   describe "::has_scanned_fingerprints_for_database_id" do
     it "should return the scanners that scanned the fingerprints in a db" do
-      db = FingerprintDatbase.where(name: "Integrated Automated Fingerprint Identification System").first
+      db = FingerprintDatabase.where(name: "Integrated Automated Fingerprint Identification System").first
       scanners = FingerprintScanner.has_scanned_fingerprints_for_database_id(db.id)
 
       scanners.count.should == 2
