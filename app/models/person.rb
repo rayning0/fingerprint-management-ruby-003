22df94c1fceb09f19b25feddae69ebd0235371ac
Spencer Rogers <spencer1248@gmail.com>
2013-11-15 11:23:39 -0500

    Fix relationships

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ActiveRecord::Base
   attr_accessible :first_name, :last_name
 
   has_many :fingerprints
-  has_many :fingerprint_databases, through: :fingerprint
-  has_many :scanned_fingerprints, through: :fingerprint
+  has_many :fingerprint_databases, through: :fingerprints
+  has_many :scanned_fingerprints, through: :fingerprints
   has_many :criminal_histories
 end
