f1fd88867ca2161c1e85cdf0bdd601677c3e5ba5
Spencer Rogers <spencer1248@gmail.com>
2013-11-15 11:01:21 -0500

    Fix spec env and add specs

--- spec/models/fingerprint_spec.rb
+++ spec/models/fingerprint_spec.rb
@@ -1,3 +1,14 @@
+require 'spec_helper'
+
 describe "Fingerprint" do
-  it "should"
+  describe "::for_offenders_of" do
+    it "should return the fingerprints with criminal histories with the given incident type" do
+      robbers = Fingerprint.for_offenders_of("Armed Robbery")
+      robbers.count.should == 2
+
+      CriminalHistory.where(incident_type: "Armed Robbery").each do |ch|
+        robbers.should include(ch.person)
+      end
+    end
+  end
 end
