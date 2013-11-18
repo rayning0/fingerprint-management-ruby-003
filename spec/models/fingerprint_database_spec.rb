cdb45673d63531d245afa6fa46fce82d1ff07d2d
Spencer Rogers <spencer1248@gmail.com>
2013-11-18 11:48:21 -0500

    Fix test

--- spec/models/fingerprint_database_spec.rb
+++ spec/models/fingerprint_database_spec.rb
@@ -5,7 +5,7 @@ describe "FingerprintDatabase" do
     it "should return the fingerprints in this database without people" do
       iafis = FingerprintDatabase.where(name: 'Integrated Automated Fingerprint Identification System').first
 
-      iafis.unknown_people_fingerpints.count.should == 1
+      iafis.unknown_people_fingerprints.count.should == 1
     end
   end
 end
