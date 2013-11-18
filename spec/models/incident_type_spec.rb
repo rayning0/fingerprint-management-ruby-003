9f2dfc5e019341e30ab8647fcaa733da4243f1ea
Spencer Rogers <spencer1248@gmail.com>
2013-11-18 11:59:44 -0500

    Fix test

--- spec/models/incident_type_spec.rb
+++ spec/models/incident_type_spec.rb
@@ -1,7 +1,7 @@
 describe "IncidentType" do
   describe "::alphabetical" do
     it "should return an alphabetical list of incident types" do
-      IncidentType.alphabetical.should ==  ["Armed Robbery", "Being Way Too Fly", "First Degree Murder", "Income Tax Evasion", "Manslaughter", "Pollywog Fighting", "Using AJAX Without a License", "Violation of the Volstead Act"]
+      IncidentType.alphabetical.map { |it| it.name }.should ==  ["Armed Robbery", "Being Way Too Fly", "First Degree Murder", "Income Tax Evasion", "Manslaughter", "Pollywog Fighting", "Using AJAX Without a License", "Violation of the Volstead Act"]
     end
   end
 end
