f22180b4e18f720344f62f62da8f9f7a0cf82489
Spencer Rogers <spencer1248@gmail.com>
2013-12-06 12:12:49 -0500

    Add test for average age

--- spec/models/incident_type_spec.rb
+++ spec/models/incident_type_spec.rb
@@ -4,4 +4,12 @@ describe "IncidentType" do
       IncidentType.alphabetical.map { |it| it.name }.should ==  ["Armed Robbery", "Being Way Too Fly", "First Degree Murder", "Income Tax Evasion", "Manslaughter", "Pollywog Fighting", "Using AJAX Without a License", "Violation of the Volstead Act"]
     end
   end
+
+  describe "#average_age_of_offenders" do
+    it "should return the average age of people with a history of the incident" do
+      armed_robbery = IncidentType.where(name: 'Armed Robbery').first
+
+      armed_robbery.average_age_of_offenders.to_f.should == 27.0
+    end
+  end
 end
